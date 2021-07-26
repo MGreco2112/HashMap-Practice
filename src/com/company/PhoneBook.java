05a5707ce3f403e945e0cfafdbb1c74b19a0f875
MGreco2112 <mgreco2112@tutanota.com>
2021-07-26 17:37:28 -0400

    Added completed PhoneBook assignment

---
+++ src/com/company/PhoneBook.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class PhoneBook {
+    public static void phoneBook() {
+        HashMap<String, Integer> phoneBook = new HashMap<>();
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            phoneBook.put(name, phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            if (phoneBook.containsKey(s)) {
+                System.out.println(s + "=" + phoneBook.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+
+}
